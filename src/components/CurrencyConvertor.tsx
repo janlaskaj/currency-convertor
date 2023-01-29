615825d9c88cb90ba4c1d82e78e55102b2c16c36
Jan Laskaj <jan.laskaj@cdn77.com>
2023-01-29 22:36:32 +0100

    add convert button

--- src/components/CurrencyConvertor.tsx
+++ src/components/CurrencyConvertor.tsx
@@ -15,15 +15,15 @@ export const CurrencyConvertor: React.FC<{}> = ({}) => {
     if (error || !data) return <span>error...</span>
 
     return (
-        <div className="flex flex-col w-full bg-white rounded-xl p-6">
-            <div className="text-gray-400 flex flex-col">
+        <div className="flex flex-col gap-4 w-full bg-white rounded-xl p-6 items-center">
+            <div className="text-gray-400 flex flex-col w-full">
                 Amount in CZK
                 <input
                     type="number"
                     className="text-gray-700 border-2 border-gray-200 shadow-sm rounded py-2 px-4 text-xl font-semibold focus:outline-gray-300"
                 />
             </div>
-            <div className="text-gray-400 flex flex-col">
+            <div className="text-gray-400 flex flex-col w-full">
                 Convert to
                 <CurrencyDropdown
                     allCurrencies={data.allCurrencies}
@@ -31,6 +31,9 @@ export const CurrencyConvertor: React.FC<{}> = ({}) => {
                     setSelectedCurrency={setSelectedCurrency}
                 />
             </div>
+            <button className="text-blue-700 font-semibold border border-blue-500 px-8 py-2 mt-2 w-44 rounded">
+                Convert
+            </button>
         </div>
     )
 }

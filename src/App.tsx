35789d1458e9a3db5f7f1d02ddb88557fcdfbbcb
Jan Laskaj <jan.laskaj@cdn77.com>
2023-01-30 00:57:40 +0100

    improve layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { Homepage } from '@src/pages'
 
 function App() {
     return (
-        <div className="h-full min-h-screen w-full bg-blue-600 p-6">
-            <h1 className="mb-4 text-5xl font-extrabold text-white">
-                Currency converter
-            </h1>
-            <h2 className="mb-4 text-white">
-                Learn the equivalent of any sum in Czech crowns in the foreign
-                currencies
-            </h2>
-            <Homepage />
+        <div className="flex h-full min-h-screen w-full flex-col items-center bg-blue-600 py-10 px-6">
+            <div className="w-full max-w-5xl">
+                <h1 className="mb-4 text-5xl font-extrabold text-white">
+                    Currency converter
+                </h1>
+                <h2 className="mb-4 text-white">
+                    Learn the equivalent of any sum in Czech crowns in the
+                    foreign currencies
+                </h2>
+                <Homepage />
+            </div>
         </div>
     )
 }

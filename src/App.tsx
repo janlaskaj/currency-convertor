66cc633d17673ab0fe1825ab6ff5c46c8dab8e2c
Jan Laskaj <jan.laskaj@cdn77.com>
2023-01-29 17:09:40 +0100

    get and parse data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { useState } from 'react'
+import { useCurrencyData } from '@src/hooks/useCurrencyData'
 
 function App() {
+    const { data, isLoading, error } = useCurrencyData()
+
+    console.log(data)
+
     return <div className="App">hello</div>
 }
 
--- src/hooks/useCurrencyData.ts
+++ src/hooks/useCurrencyData.ts
@@ -1,1 +1,58 @@
-export const test = () => console.log('test')
+import { useQuery } from 'react-query'
+
+type Currency = {
+    country: string
+    name: string
+    amount: number
+    code: string
+    rate: number
+}
+type ExchangeData = { date: string; exchangeRates: Currency[] }
+
+const proxyUrl = 'http://localhost:5001'
+const currencyDataUrl =
+    'https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt'
+const dateSeparator = '#'
+const currencySeparator = '|'
+const descriptionLineIndex = 1
+
+const parseCurrencyData = (data?: string) => {
+    if (data === undefined) return undefined
+
+    return data.split('\n').reduce<ExchangeData>(
+        (previous, current, currentIndex) => {
+            // remove last empty line and description line
+            if (current.length === 0 || currentIndex === descriptionLineIndex)
+                return previous
+
+            // first line with date
+            if (currentIndex == 0) {
+                previous.date = current.split(dateSeparator)[0]
+
+                return previous
+            }
+
+            const [country, name, amount, code, rate] =
+                current.split(currencySeparator)
+
+            previous.exchangeRates.push({
+                country,
+                name,
+                amount: Number(amount),
+                code,
+                rate: Number(rate),
+            })
+            return previous
+        },
+        { date: '', exchangeRates: [] }
+    )
+}
+
+export const useCurrencyData = () => {
+    const { data, isLoading, error } = useQuery(['currencyData'], async () => {
+        const response = await fetch(`${proxyUrl}/${currencyDataUrl}`)
+        return await response.text()
+    })
+
+    return { data: parseCurrencyData(data), isLoading, error }
+}

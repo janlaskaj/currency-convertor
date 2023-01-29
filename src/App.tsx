6fa808197d84c442e2483c907271b8f2440989c5
Jan Laskaj <jan.laskaj@cdn77.com>
2023-01-29 21:33:58 +0100

    add page description

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import { Homepage } from '@src/pages'
 function App() {
     return (
         <div className="w-full h-screen bg-blue-600 p-6">
-            <h1 className="text-white font-extrabold text-5xl">
+            <h1 className="text-white font-extrabold text-5xl mb-4">
                 Currency converter
             </h1>
+            <h2 className="text-white mb-4">
+                Learn the equivalent of any sum in Czech crowns in the foreign
+                currencies
+            </h2>
             <Homepage />
         </div>
     )

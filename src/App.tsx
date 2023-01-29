f1829a9c57b288672ffbe7de36e325a524ec7c79
Jan Laskaj <jan.laskaj@cdn77.com>
2023-01-29 18:06:47 +0100

    add headless UI and basic dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
 import { useCurrencyData } from '@src/hooks/useCurrencyData'
+import { CurrencyDropdown } from '@src/components/CurrencyDropdown'
 
 function App() {
     const { data, isLoading, error } = useCurrencyData()
 
     console.log(data)
 
-    return <div className="App">hello</div>
+    return <div className="App">{data && <CurrencyDropdown />}</div>
 }
 
 export default App

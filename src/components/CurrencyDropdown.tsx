a822d11844882df9c8d5964fcbaa6bc6ef4afeb8
Jan Laskaj <jan.laskaj@cdn77.com>
2023-01-30 01:00:11 +0100

    fix visibility

--- src/components/CurrencyDropdown.tsx
+++ src/components/CurrencyDropdown.tsx
@@ -23,7 +23,7 @@ export const CurrencyDropdown: React.FC<{
             leaveFrom="transform opacity-100 scale-100"
             leaveTo="transform opacity-0 scale-95"
         >
-            <Menu.Items className="absolute right-0 mt-2 h-60 w-24 origin-top-right divide-y divide-gray-100 overflow-scroll rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <Menu.Items className="absolute right-0 z-10 mt-2 h-60 w-24 origin-top-right divide-y divide-gray-100 overflow-scroll rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                 <div className="px-1 py-1 ">
                     {allCurrencies.map((currency) => (
                         <Menu.Item key={currency.code}>

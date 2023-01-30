2c0a029c8dcb1d3f3ad8e11c4551fe96d3c690a3
Jan Laskaj <jan.laskaj@cdn77.com>
2023-01-30 01:21:06 +0100

    styles refactor

--- src/components/CurrencyDropdown.tsx
+++ src/components/CurrencyDropdown.tsx
@@ -23,14 +23,16 @@ export const CurrencyDropdown: React.FC<{
             leaveFrom="transform opacity-100 scale-100"
             leaveTo="transform opacity-0 scale-95"
         >
-            <Menu.Items className="absolute right-0 z-10 mt-2 h-60 w-24 origin-top-right divide-y divide-gray-100 overflow-scroll rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <Menu.Items className="lg:w-30 absolute right-0 z-10 mt-2 h-60 w-24 origin-top-right divide-y divide-gray-100 overflow-scroll rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none">
                 <div className="px-1 py-1 ">
                     {allCurrencies.map((currency) => (
                         <Menu.Item key={currency.code}>
                             {({ active }) => (
                                 <button
-                                    className={`text-gray-700 ${
-                                        active ? 'bg-blue-400' : ''
+                                    className={`${
+                                        active
+                                            ? 'bg-blue-400 text-white'
+                                            : 'text-gray-700'
                                     } group flex w-full items-center justify-center rounded-md px-2 py-2 text-sm`}
                                     onClick={() =>
                                         setSelectedCurrency(currency)
